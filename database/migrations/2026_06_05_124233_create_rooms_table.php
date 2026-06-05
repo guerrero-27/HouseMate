efadaec63387b2dec64846e25ef78cba347ad644
guerrero-27 <markkevinguerrero7@gmail.com>
2026-06-05 20:53:34 +0800

    adding rooms and room image migration

---
+++ database/migrations/2026_06_05_124233_create_rooms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('room_number')->unique();
+            $table->string('room_type');
+            $table->text('description')->nullable();
+            $table->decimal('montly_rate', 8, 2);
+            $table->integer('capacity');
+            $table->integer('floor_number')->nullable();
+            $table->enum('status', ['available', 'occupied', 'maintenance'])->default('available');
+            $table->string('thumbnail')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
