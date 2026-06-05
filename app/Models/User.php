36c173d6202022abe1fa0a459bd0a020b0fc31f1
guerrero-27 <markkevinguerrero7@gmail.com>
2026-06-05 19:54:40 +0800

    modify the user models

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,22 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'profile_photo',
+        'role',
+        'status',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
     /**
      * Get the attributes that should be cast.
      *
@@ -28,5 +44,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isTenant(): bool
+    {
+        return $this->role === 'tenant';
     }
 }
